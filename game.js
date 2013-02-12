f02986e0b548f3cfb1aaa2d7c9ca0cd4ca2af779
Robin Murphy <robin@robinmurphy.co.uk>
2013-02-12 18:45:11 +0000

    Changed game example and moved src directory to main

--- game.js
+++ game.js
@@ -1,24 +1,23 @@
 (function () {    
     var gameOfLife = require('./src/game_of_life.js'),
-        game;
+        game = gameOfLife.init(5);
 
-    game = gameOfLife.init(5);
-
-    /*
-        Set up the grid
-        ___________
-        |-|-|-|-|-|
-        |-|-|-|-|-|
-        |-|X|X|X|-|
-        |-|-|-|-|-|
-        |-|-|-|-|-|
-        -----------
-    */
-
+    /**
+     *   Set up the grid:
+     *   ___________
+     *   |-|-|-|-|-|
+     *   |-|-|-|-|-|
+     *   |-|X|X|X|-|
+     *   |-|-|-|-|-|
+     *   |-|-|-|-|-|
+     *   -----------
+     */
     game.set(1, 2)
         .set(2, 2)
         .set(3, 2);
 
+    /**
+     * Start things off!
+     */
     game.start();
-
 })();
