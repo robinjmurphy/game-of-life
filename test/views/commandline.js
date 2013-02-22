5a66194b877c72f6f374851f0fb60fec3746ffb5
Robin Murphy <robin@robinmurphy.co.uk>
2013-02-22 09:03:30 +0000

    Test for update method using mock console

--- test/views/commandline.js
+++ test/views/commandline.js
@@ -13,5 +13,37 @@
         it('has an update method', function () {
             assert.ok(commandline.update);
         });
+        it('outputs the correct string for a given grid', function () {
+            var Matrix = require('../../main/lib/matrix.js'),
+                game = require('../../main/game_of_life.js'),
+                colours = require('../../main/lib/colours.js'),
+                testMatrix,
+                mockConsole,
+                expectedOutput;
+
+            testMatrix = new Matrix(2);
+            testMatrix.setAll(game.DEAD);
+            testMatrix.set(0, 0, game.ALIVE);
+
+            mockConsole = {
+                _output: '',
+                log: function (string) {
+                    this._output += string + '\n';
+                }
+            };
+
+            commandline.update({
+                time: 123,
+                grid: testMatrix.clone()
+            }, mockConsole);
+
+            expectedOutput =  'Time: 123' + '\n';
+            expectedOutput += '___' + '\n';
+            expectedOutput += colours.green('X') + '|-' + '\n';
+            expectedOutput += '-|-' + '\n';
+            expectedOutput += '---' + '\n';
+
+            assert.equal(mockConsole._output, expectedOutput);
+        });
     });
 })();
